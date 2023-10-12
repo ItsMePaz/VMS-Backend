e67912362b5f6912fd16057d2629851d0461de8b
michaeandrewpaz <146690261+michaeandrewpaz@users.noreply.github.com>
2023-10-12 12:17:58 +0800

    Commented PORT

--- utils/config.js
+++ utils/config.js
@@ -2,8 +2,8 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-const PORT = process.env.PORT;
+/* const PORT = process.env.PORT; */
 const MONGODB_URI = process.env.MONGODB_URI;
 const SECRET = process.env.SECRET;
 
-export default { PORT, MONGODB_URI, SECRET };
+export default { MONGODB_URI, SECRET };
